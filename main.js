0d201f3643bca0e52b6c0db8c68aa1fe4a51865b
makhmudoff <gachon031698@gmail.com>
2023-04-15 20:37:35 +0900

    make grade calc function

---
+++ main.js
@@ -0,0 +1,9 @@
+const calculate = () => {
+  let maths = document.getElementById("math");
+  let comp = document.getElementById("comp");
+  let phy = document.getElementById("phy");
+
+  let grades = "";
+
+  let totalgrades = parseFloat(maths) + parseFloat(comp) + parseFloat(phy);
+};
