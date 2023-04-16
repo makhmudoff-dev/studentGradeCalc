68916506aba0e238b672c18facf43566e9687524
makhmudoff <gachon031698@gmail.com>
2023-04-16 20:50:12 +0900

    first commit

--- main.js
+++ main.js
@@ -1,50 +1,57 @@
-const calc = () => {
-  let web = document.getElementById("web").value;
-  let maths = document.getElementById("maths").value;
-  let comp = document.getElementById("comp").value;
-  let phy = document.getElementById("phy").value;
+let showResult = document.getElementById("result");
+let web = document.getElementById("web");
+let maths = document.getElementById("maths");
+let comp = document.getElementById("comp");
+let phy = document.getElementById("phy");
 
-  let grades = "";
+let grades = "";
+let totalGrades = 0;
+let percentage = 0;
 
-  //   Input  is string s typecasting is necessary
+showResult.addEventListener("click", function () {
+  // Check if any input fields are empty
+  if (
+    web.value === "" ||
+    maths.value === "" ||
+    comp.value === "" ||
+    phy.value === ""
+  ) {
+    document.querySelector("#showData").innerHTML =
+      "Please enter all the fields";
+    return;
+  }
 
-  let totalGrades =
-    parseFloat(web) + parseFloat(maths) + parseFloat(comp) + parseFloat(phy);
-};
+  // Calculate the total grades and percentage
+  totalGrades =
+    parseFloat(web.value) +
+    parseFloat(maths.value) +
+    parseFloat(comp.value) +
+    parseFloat(phy.value);
+  percentage = (totalGrades / 400) * 100;
 
-// Checking the condition for the providing the grade to student based on percentage
+  // Determine the grade based on percentage
+  if (percentage >= 80) {
+    grades = "A";
+  } else if (percentage >= 60) {
+    grades = "B";
+  } else if (percentage >= 40) {
+    grades = "C";
+  } else {
+    grades = "F";
+  }
 
-let percentage = (totalgrade / 400) * 100;
-alert(percentage);
-
-debugger;
-if (totalgrade <= 100 && percentage >= 80) {
-  grades = "A";
-} else if (totalgrade <= 79 && percentage >= 60) {
-  grades = "B";
-} else if (totalgrade <= 59 && percentage >= 40) {
-  grades = "C";
-} else {
-  grades = "F";
-}
-
-// Checking the values are empty if empty than show please fill them
-
-if (web == "" || hindi == "" || maths == "" || comp == "" || phy == "") {
-  document.querySelector("#showData").innerHTML = "Please enter all the fields";
-} else {
-  // Checking the condition for the fail and pass
-  if (percentage >= 39.5) {
+  // Check if the student passed or failed
+  if (percentage >= 40) {
     document.querySelector(
       "#showData"
-    ).innerHTML = `Out of 400 your total is ${totalgrades} and 
-		percentage is ${percentage}%. <br>
-		Your grade is ${grades}. You are Pass `;
+    ).innerHTML = `Out of 400 your total is ${totalGrades} and 
+    percentage is ${percentage}%. <br>
+    Your grade is ${grades}. You passed!`;
   } else {
     document.querySelector(
       "#showData"
-    ).innerHTML = ` Out of 400 your total is  ${totalgrades} 
-        and percentage is ${percentage}%. <br> 
-        Your grade is ${grades}. You are Fail. `;
+    ).innerHTML = `Out of 400 your total is ${totalGrades} and 
+    percentage is ${percentage}%. <br>
+    Your grade is ${grades}. You failed!`;
   }
-}
+});
